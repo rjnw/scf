c1193f4127ea7902729423529bc0d18e2ab439c2
rajan <walrajan@gmail.com>
2018-03-04 22:59:32 -0500

    more changes

--- private/compiler.rkt
+++ private/compiler.rkt
@@ -9,25 +9,30 @@
   (require syntax/parse)
   (provide ast-spec language-spec)
 
-  (define-syntax-class ast-spec
-    #:description "ast specification"
-    (pattern (name:id (var:id def:ast-def) ...)))
+
 
   (define-syntax-class ast-def
     #:description "ast definition"
-    (pattern single:id)
-    (pattern ((~datum quote) datum:id))
-    (pattern (multiple:multi-ast-def ...))
-    ;; (pattern (~seq repeat:ast-def (~datum (datum->syntax #f '...))))
-    ;; (pattern (nest:ast-def))
-    )
+    ;; #:attributes (spec)
+    (pattern single:id
+             #:attr spec `(single . ,(syntax->datum #'single)))
+    (pattern ((~datum quote) datum:id)
+             #:attr spec `(quoted . ,(syntax->datum #'datum)))
+    (pattern (multiple:multi-ast-def ...)
+             #:attr spec `(multiple . ,(attribute multiple.spec))))
+
   (define-splicing-syntax-class multi-ast-def
     #:description "ast definition"
-    (pattern ms:ast-def)
-    (pattern (~seq repeat:ast-def (~datum ...)))
+    (pattern (~seq repeat:ast-def (~datum ...))
+             #:attr spec `(repeat . ,(attribute repeat.spec)))
+    (pattern ms:ast-def
+             #:attr spec (attribute ms.spec))
+)
 
-    ;; (pattern (nest:ast-def))
-    )
+  (define-syntax-class ast-spec
+    #:description "ast specification"
+    (pattern (name:id (var:id def:ast-def) ...)
+             #:attr spec (attribute def.spec)))
 
   (define-syntax-class language-spec
     #:description "language specification"
@@ -49,39 +54,38 @@
     (syntax-parse stx
       [(_ cid:id (ast nodes:ast-spec ...)
           (language langs:language-spec ...))
-       (let* (;; [info `((ast .
-              ;;              (types . ,(syntax->datum #'(nodes.name ...)))
-              ;;              ,(for/list ([names (syntax->list #'(nodes.var ... ...))]
-              ;;                          [defs  (syntax->list #'(nodes.def ... ...))])
-              ;;                 (print names defs)))  )]
-              [base-struct-id (format-id #'cid "$~a" #'cid)]
-              [base-struct  #`(struct #,base-struct-id ())])
-         (for/list ([name (syntax->list #'(nodes.name ...))]
-                    [vars (syntax->list #'((nodes.var ...) ...))]
-                    [defs  (syntax->datum #'((nodes.def ...) ...))])
-           (define group-struct-id (format-id #'cid "$~a:~a" #'cid name))
-           (define group-struct #`(struct #,group-struct-id #,base-struct-id ()))
-           (define var-structs
-             (for/list ([var (syntax->list  vars)]
-                        [def defs])
-               (define var-struct-id (format-id #'cid "$~a:~a:~a" #'cid name var))
+       (pretty-display (attribute nodes.spec))
+       #'0
+       ;; (let* ([base-struct-id (format-id #'cid "$~a" #'cid)]
+       ;;        [base-struct  #`(struct #,base-struct-id ())])
+       ;;   (for/list ([name (syntax->list #'(nodes.name ...))]
+       ;;              [vars (syntax->list #'((nodes.var ...) ...))]
+       ;;              [defs  (syntax->list #'((nodes.def ...) ...))])
+       ;;     (define group-struct-id (format-id #'cid "$~a:~a" #'cid name))
+       ;;     (define group-struct #`(struct #,group-struct-id #,base-struct-id ()))
+       ;;     (define var-structs
+       ;;       (for/list ([var (syntax->list vars)]
+       ;;                  [def (syntax->list defs)])
+       ;;         (pretty-display (attribute nodes))
+       ;;         (define var-struct-id (format-id #'cid "$~a:~a:~a" #'cid name var))
 
-               #`(struct #,var-struct-id #,group-struct-id #,def)))
-           (pretty-display (syntax->datum group-struct))
-           (pretty-display (map syntax->datum var-structs)))
+       ;;         #`(struct #,var-struct-id #,group-struct-id #,def)))
+       ;;     (pretty-display (syntax->datum group-struct))
+       ;;     (pretty-display (map syntax->datum var-structs)))
 
 
-         ;; (print (for/list ([name (syntax->datum #'(nodes.name ...))]
-         ;;                   [vars (syntax->datum #'((nodes.var ...) ...))]
-         ;;                   [defs  (syntax->datum #'((nodes.def ...) ...))])
-         ;;          (cons name (for/list ([var vars]
-         ;;                                [def defs])
-         ;;                       (cons var  def)))))
-         ;; (print #'(nodes.name ...))
-         #`(begin 42
-                  ;; #,@(ast-defines nodes)
-                  ;; #,@(language-defines langs)
-                  ))])))
+       ;;   ;; (print (for/list ([name (syntax->datum #'(nodes.name ...))]
+       ;;   ;;                   [vars (syntax->datum #'((nodes.var ...) ...))]
+       ;;   ;;                   [defs  (syntax->datum #'((nodes.def ...) ...))])
+       ;;   ;;          (cons name (for/list ([var vars]
+       ;;   ;;                                [def defs])
+       ;;   ;;                       (cons var  def)))))
+       ;;   ;; (print #'(nodes.name ...))
+       ;;   #`(begin 42
+       ;;            ;; #,@(ast-defines nodes)
+       ;;            ;; #,@(language-defines langs)
+       ;;            ))
+       ])))
 
 (require 'definer 'compiler-define-kws)
 (provide define-compiler)
