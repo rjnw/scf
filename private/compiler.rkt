3340d40f143389cb7db6943b123a1e0287d09744
Rajan <walrajan@gmail.com>
2018-04-18 02:26:50 -0400

    initial code for generating structures and printers

--- private/compiler.rkt
+++ private/compiler.rkt
@@ -20,7 +20,7 @@
              #:attr spec `(multiple . ,(attribute multiple.spec))))
 
   (define-splicing-syntax-class multi-ast-def
-    #:description "ast definition"
+    #:description "ast definition of a list of attributes"
     (pattern (~seq repeat:ast-def (~datum ...))
              #:attr spec `(repeat . ,(attribute repeat.spec)))
     (pattern ms:ast-def
@@ -35,13 +35,68 @@
     #:description "language specification"
     (pattern (lang:id (name:id var:id ...) ...))))
 
+
 (module definer racket
   (require (for-syntax (submod ".." compiler-stxclass)
+                       racket/list
+                       racket/match
+                       racket/string
                        syntax/parse
                        racket/syntax
                        racket/pretty))
   (provide define-compiler)
+  (begin-for-syntax
+    (define (get-id i)
+      (define parts (string-split (symbol->string i) ":"))
+      (string->symbol (car parts)))
+    (define (get-attrs attr-spec)
+      (match attr-spec
+        [`(single . ,s) (get-id s)]
+        [`(multiple . ,m) (map get-attrs m)]
+        [`(repeat . ,r) (get-attrs r)]
+        [`(quoted . ,_) '()]))
+    (define (get-printer spec attrs)
+      (define (get-attr-printer spec attrs k)
+        (printf "get-attr-printer: ~a ~a\n" spec attrs)
+        (match* (spec attrs)
+          [(`(single . ,s) p) (k (car p) (cdr p))]
+          [(`(multiple . ,ms) mps)
+           (get-attr-printer-multiple ms mps k)]
+          [(`(repeat . ,r) rp)
+           (get-attr-printer r rp (λ (v attrs) (k `(,v '*) attrs)))]
+          [(`(quoted . ,q) _) (k q attrs)]))
+      (define (get-attr-printer-multiple spec attrs k)
+        (printf "get-attr-printer-multiple: ~a ~a\n" spec attrs)
+        (if (empty? spec)
+            (k '() attrs)
+            (get-attr-printer
+             (car spec) attrs
+             (λ (v attrs)
+               (get-attr-printer-multiple (cdr spec) attrs (λ (nv nattrs) (k (cons v nv) nattrs))))))
+)
+      (get-attr-printer spec attrs (λ (v _) v)))
+    (get-printer
+     `(multiple . ((quoted . lambda) (multiple . ((repeat . (single . arg:terminal.sym)))) (single . body:expression)))
+     '(args body))
+    )
+
+
+
   (define-syntax (define-compiler stx)
+    (define (build-struct-defs cid full-spec)
+      #`(begin
+          #,@(for/list ([node full-spec])
+               (define node-type (car node))
+               (define node-id (format-id node-type "~a:~a" cid node-type))
+               (define node-format (format-id node-type "<~a:~a>" cid node-type))
+               (cons
+                #`(struct #,node-id () #:property prop:custom-write (λ (_) (quote #,node-format)))
+                (for/list ([attr (cdr node)])
+                  (define attr-name (car attr))
+                  (define attr-spec (cdr attr))
+
+                  (let ([attr-id (format-id attr-name "~a:~a:~a" cid node-type attr-name)])
+                    #`(struct #,attr-id #,node-id #,(flatten (get-attrs attr-spec)))))))))
     (syntax-parse stx
       [(_ cid:id (ast nodes:ast-spec ...)
           (language langs:language-spec ...))
@@ -53,6 +108,8 @@
                  (for/list ([type-var (syntax->list type-vars)]
                             [type-attribute type-attributes])
                    (cons type-var type-attribute)))))
+       (pretty-display full-spec)
+       (pretty-display (syntax->datum (build-struct-defs #'cid full-spec)))
        #`(define cid (list #,@(for/list ([node-type (syntax->list #'(nodes.name ...))]
                                          [type-vars (syntax->list #'((nodes.var ...) ...))]
                                          [type-attributes (attribute nodes.spec)])
@@ -69,15 +126,15 @@
      (expression
       [function ('lambda (arg:terminal.sym) body:expression)]
       [app (rator:expression rand:expression)]
-      [term symbol?:native-check]))
+      [term sym:native-check.symbol?]))
     (language
      (l1 (expression *))))
   (define-compiler c1
     (ast
      (terminal
-      [n number?:native-check]
-      [str string?:native-check]
-      [sym symbol?:id])
+      [num n:native-check.number?]
+      [str str:native-check.string?]
+      [sym sym:native-check.symbol?])
      (expression
       [function ('lambda (arg:terminal.sym ...) body:expression)]
       [app (rator:id args:expression ...)]
