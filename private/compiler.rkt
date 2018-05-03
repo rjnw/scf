c513e278d3492a37c44750b953d65bfecead6e11
Rajan <walrajan@gmail.com>
2018-05-03 15:36:05 -0400

    refactor

--- private/compiler.rkt
+++ private/compiler.rkt
@@ -5,11 +5,6 @@
   (require syntax/parse)
   (provide ast-spec language-spec)
 
-  (define (parse-single-def s)
-    (define s-str (symbol->string s))
-    (pretty-print s-str)
-    (match-define (list id type) (string-split s-str ":"))
-    (cons (string-split type ".") id))
   (define-syntax-class ast-def
     #:description "ast definition"
     (pattern single:id
@@ -44,7 +39,7 @@
                        syntax/parse
                        racket/syntax
                        racket/pretty))
-  (provide define-compiler)
+  (provide define-compiler define-ast)
   (begin-for-syntax
     (define (get-id i)
       (define parts (string-split (symbol->string i) ":"))
@@ -73,16 +68,32 @@
             (get-ast-writer
              (car spec) attrs
              (λ (v attrs)
-               (get-ast-writer-multiple (cdr spec) attrs (λ (nv nattrs) (k (cons v nv) nattrs)))))))
+               (get-ast-writer-multiple
+                (cdr spec) attrs
+                (λ (nv nattrs) (k (cons v nv) nattrs)))))))
       #`(define (write-proc struc port mode)
           (match-define (#,name #,@attrs) struc)
-          (display `#,(get-ast-writer spec attrs (λ (v _) v)) port))))
+          (display `#,(get-ast-writer spec attrs (λ (v _) v)) port)))
 
-
-
-  (define-syntax (define-compiler stx)
+    (define (parse-ast stx)
+      (syntax-parse stx
+        [(ast nodes:ast-spec ...)
+         (for/list ([node-type (syntax->list #'(nodes.name ...))]
+                    [type-vars (syntax->list #'((nodes.var ...) ...))]
+                    [type-attributes (attribute nodes.spec)])
+           (cons node-type
+                 (for/list ([type-var (syntax->list type-vars)]
+                            [type-attribute type-attributes])
+                   (cons type-var type-attribute))))]))
+    (define (ast-spec->syntax ast-spec)
+      #`(list #,@(for/list ([node-spec ast-spec])
+                   #`(cons (quote-syntax #,(car node-spec))
+                           (list #,@(for/list ([type-spec (cdr node-spec)])
+                                      #`(cons (quote-syntax #,(car type-spec))
+                                              (quote #,(cdr type-spec)))))))))
     (define (build-struct-defs cid full-spec)
-      (flatten (for/list ([node full-spec])
+      (flatten
+       (for/list ([node full-spec])
          (define node-type (car node))
          (define node-id (format-id node-type "~a:~a" cid node-type))
          (define node-format (format-id node-type "<~a:~a>" cid node-type))
@@ -92,43 +103,44 @@
             (define ast-name (car ast-spec-pair))
             (define ast-spec (cdr ast-spec-pair))
             (define ast-args (flatten (get-ast-args ast-spec)))
-            (define ast-args-syntax (map (λ (x) ( datum->syntax ast-name x)) ast-args))
-            (let ([attr-id (format-id ast-name "~a:~a:~a" cid node-type ast-name)])
-              (define writer (get-writer attr-id ast-spec ast-args))
-              #`(struct #,attr-id #,node-id #,ast-args
-                  #:methods gen:custom-write
-                  ( #,writer))))))))
+            (define ast-args-syntax
+              (map (λ (x) (datum->syntax ast-name x)) ast-args))
+            (define attr-id
+              (format-id ast-name "~a:~a:~a" cid node-type ast-name))
+            (define writer (get-writer attr-id ast-spec ast-args))
+            #`(struct #,attr-id #,node-id #,ast-args
+                #:methods gen:custom-write ( #,writer))))))))
+
+
+
+  (define-syntax (define-compiler stx)
     (syntax-parse stx
-      [(_ cid:id (ast nodes:ast-spec ...)
+      [(_ cid:id ast-stx:expr
           (language langs:language-spec ...))
-       (define full-spec
-         (for/list ([node-type (syntax->list #'(nodes.name ...))]
-                    [type-vars (syntax->list #'((nodes.var ...) ...))]
-                    [type-attributes (attribute nodes.spec)])
-           (cons node-type
-                 (for/list ([type-var (syntax->list type-vars)]
-                            [type-attribute type-attributes])
-                   (cons type-var type-attribute)))))
-       (define full-value
-         #`(list #,@(for/list ([node-type (syntax->list #'(nodes.name ...))]
-                               [type-vars (syntax->list #'((nodes.var ...) ...))]
-                               [type-attributes (attribute nodes.spec)])
-                      #`(cons (quote-syntax #,node-type)
-                              (list #,@(for/list ([type-var (syntax->list type-vars)]
-                                                  [type-attribute type-attributes])
-                                         #`(cons (quote-syntax #,type-var)
-                                                 (quote #,type-attribute))))))))
-       (define struct-defs (build-struct-defs #'cid full-spec))
-       (pretty-display full-spec)
-       (define defs
-         #`(begin
-             (define cid #,full-value)
-             #,@struct-defs))
-       (pretty-display (syntax->datum defs))
-       defs])))
+       (define ast-spec (parse-ast #'ast-stx))
+       (define ast-syntax (ast-spec->syntax ast-spec))
+       (define struct-defs (build-struct-defs #'cid ast-spec))
+       (pretty-display (syntax->datum ast-syntax))
+       (pretty-display (map syntax->datum struct-defs))
+      #`(begin
+          (define cid #,ast-syntax)
+          #,@struct-defs)]))
+
+  (define-syntax (define-ast stx)
+    (syntax-parse stx
+      [(_ cid:id ast-stx:expr)
+       (define ast-spec (parse-ast #'ast-stx))
+       (define ast-syntax (ast-spec->syntax ast-spec))
+       (define struct-defs (build-struct-defs #'cid ast-spec))
+       (pretty-display (syntax->datum ast-syntax))
+       (pretty-display (map syntax->datum struct-defs))
+      #`(begin
+          (define cid #,ast-syntax)
+          #,@struct-defs)])))
 
 (module test racket
   (require (submod ".." definer))
+  #;
   (define-compiler LC
     (ast
      (expression
@@ -139,7 +151,7 @@
      (l1 (expression *))))
 
 
-  (define-compiler c1
+  (define-ast c1
     (ast
      (terminal
       [num n:native-check.number?]
@@ -148,56 +160,54 @@
      (expression
       [function ('lambda (arg:terminal.sym ...) body:expression)]
       [app (rator:id args:expression ...)]
-      [term t:terminal]))
-    (language
-     (l1 (terminals *) (expr app term))))
+      [term t:terminal])))
 
-  (define-ast sham
-    (def
-      [function      (arg-ids arg-types ret-type body)]
-      [type          (type)]
-      [global        (type)]
-      [global-string (str)]
-      #:common [info #:mutable])
-    (type
-     [internal ()]
-     [ref      (to)]
-     [struct   (fields types)]
-     [function (args ret)]
-     [pointer  (to)]
-     [array    (of size)]
-     [vector   (of size)])
-    (stmt
-     [set!     (lhs:expr.var val:expr)]
-     [if       (test:expr then:stmt else:stmt)]
-     [switch   (test:expr (check:expr body:stmt) ... default)]
-     [break    ()]
-     [while    (test:expr body:stmt)]
-     [return   (value:expr)]
-     [void     ()]
-     [expr     (e:expr)]
-     [block    (stmts:stmt ...)])
-    (expr
-     [app      (rator:expr rands:expr ...)]
-     [void     ()]
-     [sizeof   (t:type)]
-     [type     (t:type)]
-     [gep      (pointer:expr indexes:expr ...)]
-     [var      (id:terminal.sym)]
-     [global   (id:terminal.sym)]
-     [external (lib-id:terminal.sym id:terminal.sym t:type)]
-     [let      ((ids:terminal.sym vals:expr types:type) ... stmt:stmt expr:expr)]
-     #:sub (const
-            [fl     (value:terminal.float        type:type)]
-            [si     (value:terminal.signed-int   type:type)]
-            [ui     (value:terminal.unsigned-int type:type)]
-            [string (value:terminal.string       type:type)]
-            [llvm   (value:terminal.llvm         type:type)]
-            [struct (value:terminal.struct       type:type)]
-            [array  (value:terminal.array        type:type)]
-            [vector (value:terminal.vector       type:type)]))
-    (rator
-     [symbol    (id:terminal.sym)]
-     [intrinsic (id:terminal.sym return-type:type)]
-     [external  (lib-id:terminal.sym id:terminal.sym ret-type:type)]
-     [racket    (id:terminal.sym racket-value:terminal.rkt full-type:type)])))
+  #;(define-ast sham
+      (def
+        [function      (arg-ids arg-types ret-type body)]
+        [type          (type)]
+        [global        (type)]
+        [global-string (str)]
+        #:common [info #:mutable])
+      (type
+       [internal ()]
+       [ref      (to)]
+       [struct   (fields types)]
+       [function (args ret)]
+       [pointer  (to)]
+       [array    (of size)]
+       [vector   (of size)])
+      (stmt
+       [set!     (lhs:expr.var val:expr)]
+       [if       (test:expr then:stmt else:stmt)]
+       [switch   (test:expr (check:expr body:stmt) ... default)]
+       [break    ()]
+       [while    (test:expr body:stmt)]
+       [return   (value:expr)]
+       [void     ()]
+       [expr     (e:expr)]
+       [block    (stmts:stmt ...)])
+      (expr
+       [app      (rator:expr rands:expr ...)]
+       [void     ()]
+       [sizeof   (t:type)]
+       [type     (t:type)]
+       [gep      (pointer:expr indexes:expr ...)]
+       [var      (id:terminal.sym)]
+       [global   (id:terminal.sym)]
+       [external (lib-id:terminal.sym id:terminal.sym t:type)]
+       [let      ((ids:terminal.sym vals:expr types:type) ... stmt:stmt expr:expr)]
+       #:sub (const
+              [fl     (value:terminal.float        type:type)]
+              [si     (value:terminal.signed-int   type:type)]
+              [ui     (value:terminal.unsigned-int type:type)]
+              [string (value:terminal.string       type:type)]
+              [llvm   (value:terminal.llvm         type:type)]
+              [struct (value:terminal.struct       type:type)]
+              [array  (value:terminal.array        type:type)]
+              [vector (value:terminal.vector       type:type)]))
+      (rator
+       [symbol    (id:terminal.sym)]
+       [intrinsic (id:terminal.sym return-type:type)]
+       [external  (lib-id:terminal.sym id:terminal.sym ret-type:type)]
+       [racket    (id:terminal.sym racket-value:terminal.rkt full-type:type)])))
