efa92fd9eebf7805a8c77558c4e6d6fb7d177705
Rajan <walrajan@gmail.com>
2018-05-11 20:48:09 -0400

    making sham's ast work including common attributes and hierarchy

--- private/compiler.rkt
+++ private/compiler.rkt
@@ -1,46 +1,51 @@
 #lang racket
 
 
-(module spec-structs racket
+(module ast-syntax-structs racket
   (provide (all-defined-out))
 
-  (struct spec:ast:group (name defs) #:prefab)
-  (struct spec:ast:node (var pat) #:prefab)
-  (struct spec:ast:pat:single   (spec) #:prefab)
-  (struct spec:ast:pat:datum    (spec) #:prefab)
-  (struct spec:ast:pat:multiple (spec) #:prefab)
-  (struct spec:ast:pat:repeat   (spec) #:prefab))
+  (struct ast:group (name parent nodes meta-info) #:prefab)
+  (struct ast:node (variable pattern meta-info) #:prefab)
+  (struct ast:pat:single   (spec) #:prefab)
+  (struct ast:pat:datum    (spec) #:prefab)
+  (struct ast:pat:multiple (spec) #:prefab)
+  (struct ast:pat:repeat   (spec) #:prefab))
 
-(module compiler-stxclass racket
+(module ast-stxclass racket
   (require syntax/parse)
   (provide ast-spec language-spec)
-  (require (submod ".." spec-structs))
+  (require (submod ".." ast-syntax-structs))
 
-  (define-syntax-class ast-def
-    #:description "ast definition"
+  (define-syntax-class node-pattern
     (pattern single:id
-             #:attr spec (spec:ast:pat:single (syntax->datum #'single)))
+             #:attr spec (ast:pat:single #'single))
     (pattern ((~datum quote) datum:id)
-             #:attr spec (spec:ast:pat:datum (syntax->datum #'datum)))
-    (pattern (multiple:multi-ast-def ...)
-            #:attr spec (spec:ast:pat:multiple (attribute multiple.spec)))
-    ;; (pattern ((~or (~seq repeat:ast-def (~datum ...)) ms:ast-def) ...)
-    ;;          #:attr spec (spec:ast:pat:multiple
-    ;;                       ((λ (s r) (if s s (map spec:ast:pat:repeat r)))
-    ;;                        (attribute ms.spec)
-    ;;                        (attribute repeat.spec))))
-    )
- (define-splicing-syntax-class multi-ast-def ;
-   #:description "ast definition of a list of attributes"
-   (pattern (~seq repeat:ast-def (~datum ...))
-            #:attr spec (spec:ast:pat:repeat (attribute repeat.spec)))
-   (pattern ms:ast-def
-            #:attr spec (attribute ms.spec)))
+             #:attr spec (ast:pat:datum #'datum))
+    (pattern (multiple:node-multiple-pattern ...)
+             #:attr spec (ast:pat:multiple (attribute multiple.spec))))
+  (define-splicing-syntax-class node-multiple-pattern
+    (pattern (~seq repeat:node-pattern (~datum ...))
+             #:attr spec (ast:pat:repeat (attribute repeat.spec)))
+    (pattern ms:node-pattern
+             #:attr spec (attribute ms.spec)))
 
-  (define-syntax-class ast-spec
-    #:description "ast specification"
-    (pattern (name:id (vars:id defs:ast-def) ...)
-             #:attr spec (spec:ast:group #'name (map spec:ast:node (syntax->list #'(vars ...)) (attribute defs.spec)))))
+  (define-splicing-syntax-class node-info
+    (pattern (~seq (~datum #:attr) v:expr)
+             #:attr as (cons 'attr #'v)))
+  (define-syntax-class ast-node
+    (pattern (var:id def:node-pattern info:node-info ...)
+             #:attr spec (ast:node #'var (attribute def.spec) (attribute info.as))))
+
+  (define-splicing-syntax-class group-info
+    (pattern (~seq (~datum #:common) v:expr)
+             #:attr as (cons 'common #'v)))
+  (define-syntax-class ast-group
+    #:description "ast group specification"
+    (pattern (name:id (~optional parent:id) nodes:ast-node ... meta:group-info ...)
+             #:attr spec (ast:group #'name (attribute parent) (attribute nodes.spec) (attribute meta.as))))
+  (define-splicing-syntax-class ast-spec
+    (pattern (~seq groups:ast-group ...)
+             #:attr spec (attribute groups.spec)))
 
   (define-syntax-class language-spec
     #:description "language specification"
@@ -48,8 +53,8 @@
 
 
 (module definer racket
-  (require (for-syntax (submod ".." compiler-stxclass)
-                       (submod ".." spec-structs)
+  (require (for-syntax (submod ".." ast-stxclass)
+                       (submod ".." ast-syntax-structs)
                        syntax/parse
                        racket/syntax
                        racket/pretty))
@@ -62,21 +67,25 @@
     (define (node-args node-pat)
       (define (rec pat)
         (match pat
-          [(spec:ast:pat:single s)  s]
-          [(spec:ast:pat:multiple m) (map rec m)]
-          [(spec:ast:pat:repeat r) (rec r)]
-          [(spec:ast:pat:datum _) '()]))
+          [(ast:pat:single s)  s]
+          [(ast:pat:multiple m) (map rec m)]
+          [(ast:pat:repeat r) (rec r)]
+          [(ast:pat:datum _) '()]))
       (rec node-pat))
 
-    (define (node-pat-format node-pat)
+    (define (node-pat-format node-pat
+                             (single (λ (v) #`,#,v))
+                             (multiple (λ (vs) #`(#,@vs)))
+                             (repeat (λ (v) #`,@`#,v))
+                             (datum (λ (v) #`#,v)))
       (define (rec pat args)
         (match* (pat args)
-          [((spec:ast:pat:single s) p) #`,#,p]
-          [((spec:ast:pat:multiple ms) mps)
-           #`(#,@(map rec ms mps))]
-          [((spec:ast:pat:repeat r) rp)
-           #`,@`#,(rec r rp)]
-          [((spec:ast:pat:datum q) _) #`#,q]))
+          [((ast:pat:single s) p) (single p)]
+          [((ast:pat:multiple ms) mps)
+           (multiple (map rec ms mps))]
+          [((ast:pat:repeat r) rp)
+           (repeat (rec r rp))]
+          [((ast:pat:datum q) _) (datum q)]))
       (rec node-pat (node-args node-pat)))
 
     (define (node-writer var pat lineage)
@@ -89,68 +98,29 @@
         [`(,groups ...)
          #`(begin
            #,@(map (curry build-defs lineage) groups))]
-        [(spec:ast:group name defs)
+        [(ast:group name parent defs meta-info)
+         (define nl (cons name (if parent (cons parent lineage) lineage)))
+         (printf "meta-info: ~a\n" meta-info)
+         (define attrs (map cdr (filter (λ (m) (equal? 'common (car m))) meta-info)))
+         (printf "attrs: ~a\n" attrs)
          #`(begin
-             (struct #,(full-id (cons name lineage)) ())
+             (struct #,(full-id nl) (#,@attrs))
              ;; (build-group-reader lineage type)
-             #,@(map (curry build-defs (cons name lineage)) defs))]
-        [(spec:ast:node var pat)
+             #,@(map (curry build-defs nl) defs))]
+        [(ast:node var pat meta-info)
          (build-node-defs var pat lineage)]))
 
     (define (build-node-defs var pat lineage)
       #`(struct #,(full-id (cons var lineage)) #,(full-id lineage) #,(flatten (node-args pat))
-          #:methods gen:custom-write (#,(node-writer var pat lineage))))
-
-    #;(define (build-defs cid full-spec)
-        (flatten
-         (for/list ([node full-spec])
-           (define node-type (car node))
-           (define node-id (format-id node-type "~a:~a" cid node-type))
-           (list
-            #`(struct #,node-id ())
-            #`(define (#,(format-id node-type "sexp->~a" node-id) s)
-                (match s
-                  #,@(for/list ([ast-spec-pair (cdr node)])
-                       (define ast-name (car ast-spec-pair))
-                       (define ast-spec (cdr ast-spec-pair))
-                       (define ast-args (get-ast-args ast-spec))
-                       (define ast-arg-types (get-ast-types ast-spec))
-                       #`(`#,(ast-format (cdr ast-spec-pair))
-                          #,@(if (and (equal? (length ast-args) 1)
-                                      (equal? (car ast-arg-types) 'native))
-                                 (list #'#:when
-                                       #`(#,(get-arg-native-check
-                                             (car (get-ast-full-args ast-spec)))
-                                          #,(car ast-args)))
-                                 '())
-                          (#,(format-id ast-name "~a:~a:~a" cid node-type ast-name)
-                           #,@ (map
-                                (λ (id type)
-                                  (if (equal? type 'native)
-                                      id
-                                      #`(#,(format-id ast-name "sexp->~a:~a"
-                                                      cid type) #,id)))
-                                ast-args ast-arg-types))))))
-            (for/list ([ast-spec-pair (cdr node)])
-              (define ast-name (car ast-spec-pair))
-              (define ast-spec (cdr ast-spec-pair))
-              (define ast-args (get-ast-args ast-spec))
-              (define ast-args-syntax
-                (map (λ (x) (datum->syntax ast-name x)) ast-args))
-              (define attr-id
-                (format-id ast-name "~a:~a:~a" cid node-type ast-name))
-              (define writer (get-writer attr-id ast-spec ast-args))
-              #`(struct #,attr-id #,node-id #,ast-args
-                  #:methods gen:custom-write ( #,writer)))))))
-    (define (parse-ast stx)
-      (syntax-parse stx
-        [(ast nodes:ast-spec ...)
-         (attribute nodes.spec)])))
+          ;; #:methods gen:custom-write (#,(node-writer var pat lineage))
+          ))
+    )
 
   (define-syntax (define-ast stx)
     (syntax-parse stx
-      [(_ cid:id ast-stx:expr)
-       (define ast-spec (parse-ast #'ast-stx))
+      [(_ cid:id gs:ast-spec)
+       (define ast-spec (attribute gs.spec))
+       ;; (pretty-display ast-spec)
        (define struct-defs (build-defs (list #'cid) ast-spec))
        (pretty-display (syntax->datum struct-defs))
        struct-defs])))
@@ -168,7 +138,7 @@
      (l1 (expression *))))
 
 
-  (define-ast c1
+  #;(define-ast c1
     (ast
      (terminal
       [num n:native.number?]
@@ -179,53 +149,55 @@
       [app (rator:expression args:expression ...)]
       [term t:terminal])))
 
-  #;(define-ast sham
-      (def
-        [function      (arg-ids arg-types ret-type body)]
-        [type          (type)]
-        [global        (type)]
-        [global-string (str)]
-        #:common [info #:mutable])
-      (type
-       [internal ()]
-       [ref      (to)]
-       [struct   (fields types)]
-       [function (args ret)]
-       [pointer  (to)]
-       [array    (of size)]
-       [vector   (of size)])
-      (stmt
-       [set!     (lhs:expr.var val:expr)]
-       [if       (test:expr then:stmt else:stmt)]
-       [switch   (test:expr (check:expr body:stmt) ... default)]
-       [break    ()]
-       [while    (test:expr body:stmt)]
-       [return   (value:expr)]
-       [void     ()]
-       [expr     (e:expr)]
-       [block    (stmts:stmt ...)])
-      (expr
-       [app      (rator:expr rands:expr ...)]
-       [void     ()]
-       [sizeof   (t:type)]
-       [type     (t:type)]
-       [gep      (pointer:expr indexes:expr ...)]
-       [var      (id:terminal.sym)]
-       [global   (id:terminal.sym)]
-       [external (lib-id:terminal.sym id:terminal.sym t:type)]
-       [let      ((ids:terminal.sym vals:expr types:type)
-                  ... stmt:stmt expr:expr)]
-       #:sub (const
-              [fl     (value:terminal.float        type:type)]
-              [si     (value:terminal.signed-int   type:type)]
-              [ui     (value:terminal.unsigned-int type:type)]
-              [string (value:terminal.string       type:type)]
-              [llvm   (value:terminal.llvm         type:type)]
-              [struct (value:terminal.struct       type:type)]
-              [array  (value:terminal.array        type:type)]
-              [vector (value:terminal.vector       type:type)]))
-      (rator
-       [symbol    (id:terminal.sym)]
-       [intrinsic (id:terminal.sym return-type:type)]
-       [external  (lib-id:terminal.sym id:terminal.sym ret-type:type)]
-       [racket    (id:terminal.sym racket-value:terminal.rkt full-type:type)])))
+  (define-ast sham
+    (def
+      [function      (arg-ids arg-types ret-type body)]
+      [type          (type)]
+      [global        (type)]
+      [global-string (str)]
+      #:common [info #:mutable])
+    (ast
+     #:common [metadata #:auto #:mutable])
+    (type ast
+          [internal ()]
+          [ref      (to)]
+          [struct   ((fields types) ...)]
+          [function (args ret)]
+          [pointer  (to)]
+          [array    (of size)]
+          [vector   (of size)])
+    (rator ast
+           [symbol    id:terminal.sym]
+           [intrinsic (id:terminal.sym return-type:type)]
+           [external  (lib-id:terminal.sym id:terminal.sym ret-type:type)]
+           [racket    (id:terminal.sym racket-value:terminal.rkt full-type:type)])
+    (stmt ast
+          [set!     (lhs:expr.var val:expr)]
+          [if       (test:expr then:stmt else:stmt)]
+          [switch   (test:expr (check:expr body:stmt) ... default)]
+          [break    ()]
+          [while    (test:expr body:stmt)]
+          [return   (value:expr)]
+          [void     ()]
+          [expr     (e:expr)]
+          [block    (stmts:stmt ...)])
+    (expr ast
+          [app      (rator:expr rands:expr ...)]
+          [void     ()]
+          [sizeof   (t:type)]
+          [etype    (t:type)]
+          [gep      (pointer:expr indexes:expr ...)]
+          [var      id:terminal.sym]
+          [global   (id:terminal.sym)]
+          [external (lib-id:terminal.sym id:terminal.sym t:type)]
+          [let      ((ids:terminal.sym vals:expr types:type) ...
+                                                             stmt:stmt expr:expr)])
+    (const expr
+           [fl     (value:terminal.float        type:type)]
+           [si     (value:terminal.signed-int   type:type)]
+           [ui     (value:terminal.unsigned-int type:type)]
+           [string (value:terminal.string       type:type)]
+           [llvm   (value:terminal.llvm         type:type)]
+           [struct (value:terminal.struct       type:type)]
+           [array  (value:terminal.array        type:type)]
+           [vector (value:terminal.vector       type:type)])))
