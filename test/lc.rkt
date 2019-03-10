90cfc824ce70bea51e70b3daf3f047b921de8644
rajan <wal.rajan@gmail.com>
2019-03-10 19:35:19 -0400

    constant fold example.

--- test/lc.rkt
+++ test/lc.rkt
@@ -1,40 +1,41 @@
 #lang racket
-(require rcf/ast)
-
+;; (require rcf/ast)
+(require "../ast.rkt")
 
 ;; old
 (define-ast LC
-  ;; #:custom-write #t
+  #:custom-write #t
   #:build-map #t
+  #:prefix ||
+  #:top-seperator ||
+  #:seperator -
   (expr
-   [lambda ((x:terminal.sym) body:expr)]
+   [lambda ((x:terminal.sym ...) body:expr)]
    [letrec (((ids:terminal.sym vals:expr) ...) body:expr)]
-   [app (rator:expr rand:expr)]
-   [sym s:terminal.sym])
+   [app (rator:expr rand:expr ...)]
+   [term v:terminal])
   (terminal #:terminals
             [n number?]
             [sym symbol?]))
 
-;; (define LC2
-;;   (extend-ast
-;;    LC
-;;    (expr
-;;     ('let ((ids:terminal.sym vals:expr) ...) e:expr)
-;;     ('begin e:expr ...))
-;;    (stmt
-;;     ('set! id:terminal.sym val:expr))))
+(define ex1 (expr-app (expr-term '+) (list (expr-term 2) (expr-term 3))))
+(define ex2 (expr-app (expr-term '*) (list (expr-term 2) (expr-app (expr-term '+)
+                                                                   (list (expr-term 3)
+                                                                         (expr-term 4))))))
+(define (plus a b) (expr-app (expr-term '+) (list a b)))
+(define (mult a b) (expr-app (expr-term '*) (list a b)))
+(define t expr-term)
+(define ex3 (expr-lambda '(a) (plus (t 'a) (plus (t 2) (t 3)))))
+(define (constant-fold e)
+  (match (map-expr constant-fold identity e)
+    [(expr-app (expr-term '+) (list (expr-term v1) (expr-term v2)))
+     #:when (and (number? v1) (number? v2))
+     (expr-term (+ v1 v2))]
+    [(expr-app (expr-term '*) (list (expr-term v1) (expr-term v2)))
+     #:when (and (number? v1) (number? v2))
+     (expr-term (* v1 v2))]
+    [e e]))
 
-;; (define-language L1 #:ast LC2
-;;   (expr * (- let))
-;;   (terminal n sym))
-
-;; (define-language L2 #:ast LC2 #:extend L1
-;;   (+ (stmt *)))
-
-;; (define-pass P1 (L2 -> L1)
-;;   [(lambda ())])
-
-
-;; ($LC:expr `(lambda (n) 123))
-;; ($LC:expr `(lambda (12) 123))
-;; ($LC:expr `(letrec ([a 1] [b 2]) b))
+(printf "before: ~a, after: ~a\n" ex1 (constant-fold ex1))
+(printf "before: ~a, after: ~a\n" ex2 (constant-fold ex2))
+(printf "before: ~a, after: ~a\n" ex3 (constant-fold ex3))
